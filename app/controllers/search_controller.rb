61a65119a1a1ae7359edcca53b65391fdd5fa5d9
matanskii <matan@clevertech.biz>
2015-12-17 17:07:45 -0500

    working table

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,29 @@
-require 'twitter_search'
-class SearchController < ApplicationController
+ require 'twitter'
+ class SearchController < ApplicationController
 
 	def welcome
 		render 'welcome'
 	end
 
 	def search_twitter
-		client = TwitterSearch::Client.new(‘Tweetomator‘)
-		tweets = client.query(params[:search_text])
+
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = "UZ13uS8QRgcs6wwk6ALjgBO6D"
+		  config.consumer_secret     = "XWthniNCrugaJzE2otKG8hHCaM8yJCdFOYJIoeIvfToTBRqhah"
+		  config.access_token        = "4501808979-vJPDaFSmmAXa5iXnlkUbo9sFxINbsMrocukgwjT"
+		  config.access_token_secret = "mkSHMkGtCUHvWwEiHv90esflqmf9mLO8Fa9oBkemAzb53"
+		end
+		
+		@tweets = []
+		client.search("marry me", :result_type => "recent").take(5).each do |tweet|
+			retweet = tweet.text.split[0]=="RT"
+			components = {
+				user: retweet ? tweet.text.split[1] : tweet.text.split[0],
+				tweet: tweet.text[tweet.text.index(":").to_i..-1],
+				retweet: retweet
+			}
+			@tweets << components
+		end
 		render 'results'
 	end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,10 @@
 class Search < ActiveRecord::Base
+
+	field :query
+	field :results, type: Array, defualt: Array.new
+
+	belongs_to :user
+
+	
+
 end
